690f36701eedee07b39d032ad01480d8a4b19ae4
Wojciech Balda <wojtek.balda@gmail.com>
2024-01-03 22:14:33 +0100

    update config and install/uninstall some packages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    webpack(config) {
+        config.module.rules.push({
+          test: /\.svg$/i,
+          use: [{loader: '@svgr/webpack', options: {icon: true}}],
+        })
+    
+        return config
+      },
+      images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'media.graphassets.com',
+            port: '',
+          },
+        ],
+      },
+}
