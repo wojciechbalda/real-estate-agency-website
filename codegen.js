69933f8e3037c41d2ed9c3d41a1b72b584ba978b
Wojciech Balda <wojtek.balda@gmail.com>
2024-01-03 22:13:35 +0100

    update codegen config

--- codegen.js
+++ codegen.js
@@ -8,7 +8,7 @@ module.exports = {
   schema: process.env.HYGRAPH_TYPES_ENDPOINT,
   generates: {
     'gen-types.ts': {
-      documents: './app/lib/fetch.ts',
+      documents: './app/lib/fetch.ts',     
       plugins: [
         'typescript',
         'typescript-operations'
