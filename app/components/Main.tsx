950e4b3cb32e81325d90456dd96f32ae1a3a6f4d
Wojciech Balda <wojtek.balda@gmail.com>
2024-01-03 22:20:35 +0100

    Fix main component

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -3,7 +3,7 @@ type MainProps = {
 }
 
 const Main = ({children}: MainProps) => {
-    return <main className="max-w-6xl mx-auto px-3 grid gap-4">
+    return <main className="max-w-7xl mx-auto px-6 grid gap-4">
         {children}
     </main>
 }
